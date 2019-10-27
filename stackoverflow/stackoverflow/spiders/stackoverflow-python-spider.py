df13afa72bcd083a3ece7abee953ccc1b62bfe51
weiyuanchuang <luobo157@126.com>
2019-10-27 17:37:39 +0800

    Update stackoverflow-python-spider.py
    items.py文件里的是StackoverflowItem，所以应该需要修改下

--- stackoverflow/stackoverflow/spiders/stackoverflow-python-spider.py
+++ stackoverflow/stackoverflow/spiders/stackoverflow-python-spider.py
@@ -1,5 +1,5 @@
 import scrapy
-from stackoverflow.items import StackoverflowPythonItem
+from stackoverflow.items import StackoverflowItem
 
 class StackoverflowPythonSpider(scrapy.Spider):
 
@@ -19,7 +19,7 @@ class StackoverflowPythonSpider(scrapy.Spider):
         question_list = response.xpath('//*[@id="questions"]')
 
         for question in question_list.xpath('./div'):
-            item = StackoverflowPythonItem()
+            item = StackoverflowItem()
             item['_id'] = question.attrib['id']
             item['questions'] = question.xpath('div[2]/h3/a/text()').extract()
             item['votes'] = question.xpath(
